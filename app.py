666a257dfb088cef667f479ad5a15db9aa3bb7c1
k018c1277 <k018c1277@it-neec.jp>
2020-08-31 16:45:54 +0900

    3.2.template

--- app.py
+++ app.py
@@ -1,41 +1,11 @@
-from flask import Flask, render_template, request, redirect
-import mysql.connector as db
-
-db_param  = {
-    'user' : 'mysql',
-    'host' : 'localhost',
-    'password' : '',
-    'database' : 'db1'
-}
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    conn = db.connect(**db_param)
-    cur = conn.cursor()
-    stmt = 'SELECT * FROM books'
-    cur.execute(stmt)
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', books=rows)
+    return render_template('index.html', title="タイトル")
 
-@app.route('/send', methods=[POST])
-def send():
-    title = request.form.get('title')
-    price = request.form.get('price')
-    if title =="" or price=="":
-        return redirect('/')
-    conn = db.connect(**db_param)
-    cur = conn.cursor()
-    stmt = 'INSERT INTO books(title, price) VALUES(%s, %s)'
-    cur.execute(stmt, (title, int(price)))
-    conn.commit()
-    cur.close()
-    conn.close
-    return redirect('/')
-
-if __name__=='__main__':
-        app.debug = True
-        app.run()
+if __name__ == '__main__' :
+    app.debug = True
+    app.run()
