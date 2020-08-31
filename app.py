cf2386c12ccf3b27fb7af9badde27e1550bb1e3f
k018c1277 <k018c1277@it-neec.jp>
2020-08-31 19:00:23 +0900

    3.6.template( for_dic )

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', list=["アイテム1", "アイテム2", "アイテム3"])
-
+    list = {'apple':'リンゴ', 'orange':'みかん', 'lemon':'レモン'}
+    return render_template('index.html', list=list)
 if __name__ == '__main__' :
     app.debug = True
     app.run()
