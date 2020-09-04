e37e19b65b3115105bd4baeee5e1143b5366ab07
k018c1277 <k018c1277@it-neec.jp>
2020-09-05 01:13:26 +0900

    5.1.http_method

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-data = []
 
-@app.route('/user/<id>/')
-def adduser(id):
-        data.append(id)
-        return render_template('index.html', user=id)
+@app.route('/')
+def index():
+        return render_template('index.html')
 
-@app.route('/list/')
-def userlist():
-        return render_template('list.html',  list=data)
+@app.route('/send', methods=['POST'])
+def send():
+        msg = request.form.get('msg')
+        return render_template('receive.html',  message = msg)
 
 if __name__ == '__main__':
     app.debug = True
