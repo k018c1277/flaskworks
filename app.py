3180347ab624667954182c2a80b2272dbebc2d63
k018c1277 <k018c1277@it-neec.jp>
2020-09-04 23:20:27 +0900

    4.1.kadai

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 data = []
 
 @app.route('/user/<id>/')
-def tuika(id):
+def adduser(id):
         data.append(id)
         return render_template('index.html', user=id)
 
 @app.route('/list/')
-def itiran():
+def userlist():
         return render_template('list.html',  list=data)
 
 if __name__ == '__main__':
