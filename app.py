3d34aaf8e0d44d62e899fc1df5bdd1c2d9679687
k018c1277 <k018c1277@it-neec.jp>
2020-09-04 20:27:48 +0900

    3.5.template(for)

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-data = ["AAA", "BBB", "CCC", "DDD"]
+data = []
 
-@app.route('/post/<int:post_id>/')
-def profile(post_id):
-    if post_id < len(data):
-        return render_template('index.html', message=data[post_id])
-    else:
-        return "0 ~ " + str(len(data)-1) + " の値を入力してください"
+@app.route('/user/<id>/')
+def tuika(id):
+        data.append(id)
+        return render_template('index.html', user=id)
+
+@app.route('/list/')
+def itiran():
+        return render_template('list.html',  list=data)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
