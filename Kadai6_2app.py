4559c57fa9e429e1235f43ff689d74dbd950d931
k018c1277 <k018c1277@it-neec.jp>
2020-09-06 01:29:25 +0900

    6.2.image_upload(sec)

---
+++ Kadai6_2app.py
@@ -0,0 +1,26 @@
+from  flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'git'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('Kadai6_2index.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    img_file = request.files['img_file']
+    if img_file and allowed_file(img_file.filename):
+        img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
+        return '<p>画像' + img_file.filename + 'を送信しました</p>'
+    else:
+        return '<p>許可されていない拡張子です</p>'
+
+if __name__=='__main__':
+    app.debug = True
+    app.run()
