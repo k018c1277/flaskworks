33136326469e048966eb0dee8615be76b8b23491
k018c1277 <k018c1277@it-neec.jp>
2020-09-06 01:02:10 +0900

    5.1.image_upload

---
+++ Kadai6_1app.py
@@ -0,0 +1,18 @@
+from  flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
+
+@app.route('/')
+def index():
+    return render_template('Kadai6_1index.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    img_file = request.files['img_file']
+    img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
+    return '<p>画像' + img_file.filename + 'を送信しました</p>'
+
+if __name__=='__main__':
+    app.debug = True
+    app.run()
