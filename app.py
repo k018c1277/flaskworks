ff357b7fbae182a4a8b1b571ba92c4c451b87f3c
k018c1277 <k018c1277@it-neec.jp>
2020-09-14 14:03:33 +0900

    9.5.database(webapi)

--- app.py
+++ app.py
@@ -1,60 +1,96 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import mysql.connector as db
+import os
 
 db_param  = {
     'user' : 'mysql',
     'host' : 'localhost',
     'password' : '',
-    'database' : 'db1'
+    'database' : 'itemdb'
 }
 
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = './static/uploads'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     conn = db.connect(**db_param)
     cur = conn.cursor()
-    stmt = 'SELECT * FROM books'
+    stmt = 'SELECT * FROM list'
     cur.execute(stmt)
     rows = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('index.html', books=rows)
+    return render_template('index.html', list=rows)
 
-@app.route('/send', methods={'POST'})
+@app.route('/send', methods=['POST'])
 def send():
+        print(request.files)
         title = request.form.get('title')
         price = request.form.get('price')
-        if title=="" or price=="":
-                return redirect('/')
+        image = request.files['img_file']
+        if title=="" or price=="" or image=="":
+            return redirect('/')
+
+        if image and allowed_file(image.filename):
+            image.save('static/uploads/' + image.filename)
         conn = db.connect(**db_param)
         cur = conn.cursor()
 
-        stmt = 'SELECT * FROM books WHERE title=%s'
+        stmt = 'SELECT * FROM list WHERE title=%s'
         cur.execute(stmt, (title,))
         rows = cur.fetchall()
         if len(rows)==0:
-            cur.execute('INSERT INTO books(title, price) VALUES(%s, %s)', (title, int(price)))
+            cur.execute('INSERT INTO list(title, price, image) VALUES(%s, %s, %s)', (title, int(price), image.filename))
         else:
-            cur.execute('UPDATE books SET price=%s WHERE title=%s', (int(price), title))
+            cur.execute('UPDATE list SET price=%s, image=%s WHERE title=%s', (int(price), image.filename, title))
         conn.commit()
         cur.close()
         conn.close()
         return redirect('/')
+
 
 @app.route('/delete', methods=['POST'])
 def delete():
     del_list = request.form.getlist('del_list')
     conn = db.connect(**db_param)
     cur = conn.cursor()
-    stmt = 'DELETE FROM books WHERE id=%s'
     for id in del_list:
-            cur.execute(stmt,(id,))
+        stmt = 'SELECT * FROM list WHERE id=%s'
+        cur.execute(stmt,(id,))
+        rows = cur.fetchall()
+        os.remove('./static/uploads/' + rows[0][3])
+        stmt = 'DELETE FROM list WHERE id=%s'
+        cur.execute(stmt, (id,))
     conn.commit()
     cur.close()
     conn.close()
     return redirect('/')
-    
+
+@app.route('/data', methods=['GET'])
+def data():
+    keyword = request.args.get('keyword')
+    conn = db.connect(**db_param)
+    cur = conn.cursor()
+    if keyword and keyword != "":
+        stmt = 'SELECT * FROM list WHERE title LIKE %s'
+        cur.execute(stmt, ('%'+keyword+'%',))
+    else:
+        stmt = 'SELECT * FROM list'
+        cur.execute(stmt)
+    rows = cur.fetchall()
+    url = 'http://127.0.0.1:5000/static/uploads/'
+    data = []
+    for id, title, price, image in rows:
+        data.append({'id':id, 'title':title, 'price':price, 'image':url+image})
+    ret = jsonify({"result":data})
+    return ret
+
 if __name__=='__main__':
         app.debug = True
         app.run()
